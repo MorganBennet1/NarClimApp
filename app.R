501a58a090c5c5e83bd65d353a8b2e71ed9bda5a
MorganBennet1 <145090611+MorganBennet1@users.noreply.github.com>
2026-09-09 09:06:40 +1000

    Update app.R
    Add .global for publishing app.R

--- app.R
+++ app.R
@@ -1,3 +1,26 @@
+library(shiny)
+library(leaflet)
+library(ncdf4)
+library(dplyr)
+library(purrr)
+library(readr)
+library(tibble)
+library(rsconnect)
+
+rsconnect::writeManifest()
+
+netcdf_root <- "NetCDF"
+
+variables <- c("R99p")
+
+periods <- as.character(
+  seq(
+    2030,
+    2090,
+    by = 5
+  )
+)
+
 #========================================================
 # UI
 #========================================================
