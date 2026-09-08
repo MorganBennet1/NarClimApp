d615a89a4f2935dd3be9594d2e822dde8b58dd19
MorganBennet1 <145090611+MorganBennet1@users.noreply.github.com>
2026-09-08 17:11:43 +1000

    Create global.R

---
+++ global.R
@@ -0,0 +1,19 @@
+library(shiny)
+library(leaflet)
+library(ncdf4)
+library(dplyr)
+library(purrr)
+library(readr)
+library(tibble)
+
+netcdf_root <- "NetCDF"
+
+variables <- c("R99p")
+
+periods <- as.character(
+  seq(
+    2030,
+    2090,
+    by = 5
+  )
+)
