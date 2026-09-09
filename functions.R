7ae75e59b1fca578ed87ef0b2509922f2d0a2ef7
MorganBennet1 <145090611+MorganBennet1@users.noreply.github.com>
2026-09-09 14:11:51 +1000

    Update functions.R
    Added function to extract area-weighted mean

--- functions.R
+++ functions.R
@@ -85,3 +85,100 @@ extract_point <- function(
     }
   )
 }
+
+extract_area_mean <- function(
+    ncfile,
+    xmin,
+    xmax,
+    ymin,
+    ymax,
+    periods_selected
+){
+
+  nc <- nc_open(ncfile)
+
+  on.exit(
+    nc_close(nc)
+  )
+
+  var_name <- names(
+    nc$var
+  )[1]
+
+  vals <- ncvar_get(
+    nc,
+    var_name
+  )
+
+  dim_names <- names(
+    nc$dim
+  )
+
+  lon_name <- dim_names[
+    grepl(
+      "lon|longitude|x",
+      tolower(dim_names)
+    )
+  ][1]
+
+  lat_name <- dim_names[
+    grepl(
+      "lat|latitude|y",
+      tolower(dim_names)
+    )
+  ][1]
+
+  lons <- nc$dim[[lon_name]]$vals
+  lats <- nc$dim[[lat_name]]$vals
+
+  period_vals <- nc$dim$period$vals
+
+  lon_idx <- which(
+    lons >= xmin &
+      lons <= xmax
+  )
+
+  lat_idx <- which(
+    lats >= ymin &
+      lats <= ymax
+  )
+
+  weights <- cos(
+    lats[lat_idx] *
+      pi / 180
+  )
+
+  map_df(
+    periods_selected,
+    function(period){
+
+      pindex <- which(
+        period_vals ==
+          as.numeric(period)
+      )
+
+      area_vals <- vals[
+        lon_idx,
+        lat_idx,
+        pindex
+      ]
+
+      tibble(
+        Period = period,
+
+        Value = weighted.mean(
+          x = as.vector(area_vals),
+
+          w = rep(
+            weights,
+            each = length(lon_idx)
+          ),
+
+          na.rm = TRUE
+        )
+      )
+
+    }
+  )
+
+}
